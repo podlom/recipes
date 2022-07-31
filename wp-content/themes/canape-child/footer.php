bec92a19bd36667a37e2e9c4c8bc80f7db080b39
Shkodenko V. Taras <taras.shkodenko@blackbit.de>
2022-07-31 18:30:57 +0300

    Customized child theme footer

---
+++ wp-content/themes/canape-child/footer.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Canape-child
+ */
+
+?>
+
+	</div><!-- #content -->
+
+		<?php get_sidebar( 'footer' ); ?>
+
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="site-info">
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'canape' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'canapechild' ), 'WordPress' ); ?></a>
+				<span class="sep"> | </span>
+				<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'canapechild' ), 'canape-child', '<a href="https://www.shkodenko.com/" rel="developer">Taras Shkodenko</a>' ); ?>
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
