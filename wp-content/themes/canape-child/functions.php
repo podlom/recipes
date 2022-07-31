b14c1df7567b67473af7b516b7349d0eaa43bf11
Shkodenko V. Taras <taras.shkodenko@blackbit.de>
2022-07-31 19:02:50 +0300

    Improved canape-child translation to uk

--- wp-content/themes/canape-child/functions.php
+++ wp-content/themes/canape-child/functions.php
@@ -7,4 +7,9 @@
  * @package Canape-child
  */
 
-load_theme_textdomain( 'canapechild', get_template_directory() . '/languages' );
+add_action('after_setup_theme', 'canapechild_theme_setup');
+function canapechild_theme_setup()
+{
+    /** @see: https://wordpress.stackexchange.com/questions/127917/generating-po-mo-translating-files-from-scratch-in-a-wordpress-theme */
+    load_theme_textdomain('canapechild', get_template_directory() . '/languages');
+}
